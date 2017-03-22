73de7ec99ca0695d3f8b75aed732974964e52c96
Toon Van de Putte <toon@automaton.be>
2017-03-22 23:38:10 +0100

    First complete landing page
    - mailinglist subscribe
    - photo slideshow

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 var minifyCSS = require('gulp-minify-css');
 var del = require('del');
+var rename = require("gulp-rename");
 
 gulp.task('default', function() {
     gulp.src(['less/style.less'])
--- js/main.js
+++ js/main.js
@@ -14,11 +14,11 @@ $( document ).ready(function() {
         var hours = Math.floor( (timediff - 86400000*days)/3600000);
         var minutes = Math.floor( (timediff-86400000*days-3600000*hours)/60000 );
         var seconds = Math.floor( (timediff-86400000*days-3600000*hours-60000*minutes)/1000 );
-        console.log(days);
+        /*console.log(days);
         console.log(hours);
         console.log(minutes);
         console.log(seconds);
-        console.log(timediff);
+        console.log(timediff);*/
 
         ("00" + 1234).slice (-3)
 
@@ -28,5 +28,20 @@ $( document ).ready(function() {
         $(".seconds .val").html(("00"+seconds).slice(-2));
       }
     }
+    //countdown timer
     setInterval(setClock, 1000);
+    //slideshow
+    $('.slideshow').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      variableWidth: true,
+      dots: false,
+      infinite: true,
+      speed: 1000,
+      fade: false,
+      cssEase: 'ease-in-out',
+      centerMode: true,
+      autoplay: true,
+      autoplaySpeed: 2000,
+    });
 });
