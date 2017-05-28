8b0d82b9220a5541409d986b481a42e0a25293ae
Toon Van de Putte <toon@automaton.be>
2017-05-28 20:49:00 +0200

    Info over locatie toegevoegd

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
     // slideshow
-    $('.slideshow').slick({
+    $('.slideshowwrapper-sfeer .slideshow').slick({
       lazyLoad: 'ondemand',
       slidesToShow: 3,
       slidesToScroll: 1,
@@ -12,6 +12,30 @@ $( document ).ready(function() {
       cssEase: 'ease-in-out',
       centerMode: true,
       autoplay: true,
+      autoplaySpeed: 2000
+    });
+    $('.slideshowwrapper-prospectie .slideshow').slick({
+      lazyLoad: 'ondemand',
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      variableWidth: true,
+      arrows: false,
+      dots: true,
+      infinite: true,
+      speed: 1000,
+      fade: false,
+      cssEase: 'ease-in-out',
+      centerMode: false,
+      autoplay: false,
       autoplaySpeed: 2000,
+      responsive: [
+      {
+        breakpoint: 400,
+        settings: {
+          dots: false,
+          arrows: true
+        }
+      }
+    ]
     });
 });
