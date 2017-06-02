4c8982bf72dc78e7121b098145c79f5a6c14d9cf
Toon Van de Putte <toon@automaton.be>
2017-06-02 23:58:15 +0200

    Content update
    - extra slideshow ivm prospectie
    -

--- js/main.js
+++ js/main.js
@@ -1,19 +1,20 @@
 $( document ).ready(function() {
     // slideshow
     $('.slideshow').slick({
-      lazyLoad: 'ondemand',
+      lazyLoad: 'progressive',
       slidesToShow: 3,
       slidesToScroll: 1,
       variableWidth: true,
       dots: false,
       infinite: true,
       speed: 1000,
+      initialSlide: 0,
       fade: false,
       cssEase: 'ease-in-out',
       centerMode: true,
       autoplay: true,
-      autoplaySpeed: 2000,
-    });
+      autoplaySpeed: 2000
+    })
 });
 
 $(window).scroll(function() {
