cc0b83ab904741eb4cd3dd638c62dae8cfe2c541
Toon Van de Putte <toon@automaton.be>
2017-09-23 23:47:35 +0200

    Looping implemented

--- js/slideshow.js
+++ js/slideshow.js
@@ -6,81 +6,92 @@ $(document).ready(function(){
             function(i) {
                 tw += parseInt($(this).width(), 10);
                 tw += parseInt($(this).css('margin-right'))*2; // 20px margin on each side per image
-                console.log(parseInt($(this).css('margin-right'))*2);
             }
         );
-        console.log(tw);
 
         return tw;
     }
 
-    function slideshow_goto( elem, slideto = 1, animate = true ) {
+    function slideshow_init( elem, focusnr = 1 ) {
         var viewportwidth = $( elem ).width();
         var imageslist = $( elem ).find( ".images" );
-        var total = $( elem ).find(".images li").length;
-        var slidefrom = $( elem ).find(".images li.selected").data("volgnr");
-        if ( !slidefrom ) {
-            slidefrom = 1;
+        var total = $( elem ).find(".images li").not(".clone").length;
+        if (focusnr > total) {
+            focusnr = focusnr % total;
         }
-        $( elem ).find(".images li.selected").removeClass("selected");
-        $( elem ).find(".images li.previtem").removeClass("previtem");
-        $( elem ).find(".images li.nextitem").removeClass("nextitem");
-        //console.log($( elem ).find(".images li").length);
-        if (slideto > total) {
-            slideto = slideto % total;
-        }
-        var curritem = $( elem ).find(".images li[data-volgnr="+slideto+"]");
-        curritem.addClass("selected");
-        var itemmargin = parseInt(curritem.css('margin-right'));
-        console.log(itemmargin);
-        var itemwidth = curritem.width()+(itemmargin*2);
+        var itemmargin = parseInt($( elem ).find(".images li[data-volgnr=1]").css('margin-right'));
+        var itemwidth = $( elem ).find(".images li[data-volgnr=1]").width()+(itemmargin*2);
         var leftpos = (viewportwidth - itemwidth) / 2;
-        $( elem ).find(".images li").sort(function(a,b) {
-             return a.dataset.volgnr > b.dataset.volgnr;
-        }).appendTo( imageslist );
-        if ( 1 == slideto ) {
-            var previtem = $( elem ).find(".images li[data-volgnr="+total+"]");
-            var nextitem = $( elem ).find(".images li[data-volgnr=2]");
-            var shiftleft = leftpos - itemwidth*slideto;
-            //previtem.prependTo( imageslist );
-        } else if ( total == slideto ) {
-            var previtem = $( elem ).find(".images li[data-volgnr="+(total-1)+"]");
-            var nextitem = $( elem ).find(".images li[data-volgnr=1]");
-            var shiftleft = leftpos - itemwidth*(slideto-2);
-            //nextitem.appendTo( imageslist );
-        } else {
-            var previtem = curritem.prev("li");
-            var nextitem = curritem.next("li");
-            var shiftleft = leftpos - itemwidth*(slideto-1);
-        }
-        var shiftleft = leftpos - itemwidth*(slideto-1);
-        var shiftdirection = 1;
-        if ( (slidefrom == total && 1 == slideto) || -1 == slidefrom - slideto ){
-            shiftdirection = -1;
-        } else if (slidefrom == slideto) {
-            shiftdirection = 0;
-        }
-        previtem.addClass("previtem");
-        nextitem.addClass("nextitem");
-        curritem.children("img").attr("src", curritem.children("img").data("src"));
-        previtem.children("img").attr("src", previtem.children("img").data("src"));
-        nextitem.children("img").attr("src", nextitem.children("img").data("src"));
+        imageslist.data('total',total);
+        imageslist.data('viewportwidth',viewportwidth);
+        imageslist.data('itemmargin',itemmargin);
+        imageslist.data('itemwidth',itemwidth);
+        imageslist.data('leftpos',leftpos);
+        imageslist.data('focusnr',focusnr);
+        imageslist.children().remove( ".clone" );
+        $( elem ).find(".images li[data-volgnr="+focusnr+"]").addClass("selected");
+        $( elem ).find(".images li[data-volgnr=1]").clone(true).appendTo( imageslist ).addClass( "clone" );
+        $( elem ).find(".images li[data-volgnr=2]").clone(true).appendTo( imageslist ).addClass( "clone" );
+        $( elem ).find(".images li[data-volgnr="+total+"]").clone(true).prependTo( imageslist ).addClass( "clone" );
+        $( elem ).find(".images li[data-volgnr="+(total-1)+"]").clone(true).prependTo( imageslist ).addClass( "clone" );
         set_totalwidth( elem );
-        //console.log(shiftleft);
-        var speed = 0;
-        if ( true == animate ) {
-            imageslist.animate(
-                { 'left' : '+='+ (itemwidth*shiftdirection) + 'px'},
-                500,
-                function( o = $(this) ) {
-                    console.log("animated"+o.attr('class'));
-                    slideshow_goto( $(this).parents(".fri3d-slideshowwrapper"), $(this).children(".selected").data("volgnr"), false );
-                }
-            )
+        set_srcs( elem, focusnr );
+        set_leftpos ( elem, focusnr );
+    }
+
+    function set_leftpos ( elem, focusnr = 1 ) {
+        var imageslist = $( elem ).find( ".images" );
+        var shiftleft = imageslist.data('leftpos') - imageslist.data('itemwidth')*focusnr - imageslist.data('itemwidth');
+        imageslist.css('left',shiftleft);
+        set_navclasses( elem, focusnr );
+    }
+
+    function set_navclasses( elem, focusnr ) {
+        clear_tempclasses( elem );
+        curritem = $( elem ).find("li[data-volgnr="+focusnr+"]").addClass("selected");
+        curritem.prev("li").data('dir','left');
+        curritem.next("li").data('dir','right');
+    }
+
+    function clear_tempclasses( elem ){
+        $( elem ).find("li").removeClass("selected");
+        $( elem ).find("li").removeClass("left");
+        $( elem ).find("li").removeClass("right");
+    }
+
+    function set_srcs( elem, center = 1 ) {
+        var imageslist = $( elem ).find(".images");
+        centeritem = $( elem ).find("li[data-volgnr="+center+"]");
+        if ( 1 == center ) {
+            leftitem = $( elem ).find("li[data-volgnr="+ imageslist.data('total') +"]");
         } else {
-            imageslist.css('left',shiftleft);
+            leftitem = $( elem ).find("li[data-volgnr="+( center-1 )+"]");
         }
-        //console.log( previtem );
+        if ( imageslist.data('total') == center ) {
+            rightitem = $( elem ).find("li[data-volgnr=1]");
+        } else {
+            rightitem = $( elem ).find("li[data-volgnr="+( center+1 )+"]");
+        }
+        centeritem.children("img").attr("src", centeritem.children("img").data("src"));
+        leftitem.children("img").attr("src", leftitem.children("img").data("src"));
+        rightitem.children("img").attr("src", rightitem.children("img").data("src"));
+    }
+
+    function slideshow_slide( elem, dir = 'right' ) {
+        $( elem ).addClass("noclick");
+        var shiftdirection = -1;
+        if ( 'left' == dir ) {
+            var shiftdirection = 1;
+        }
+        var distance = $( elem ).data('itemwidth')*shiftdirection;
+        $( elem ).animate(
+            { 'left' : '+='+ ($( elem ).data('itemwidth')*shiftdirection) + 'px'},
+            500,
+            function( o = $(this).parents(".fri3d-slideshowwrapper") ) {
+                set_leftpos( o, o.find(".images").data('nextnr') );
+                o.find(".images").removeClass('noclick');
+            }
+        );
     }
 
     function set_totalwidth( elem ) {
@@ -94,17 +105,18 @@ $(document).ready(function(){
                 if (!volgnr) {
                     volgnr = 1;
                 }
-                slideshow_goto( $(this), volgnr, false );
+                slideshow_init( $(this), volgnr );
             }
         );
     }
 
     $(".fri3d-slideshowwrapper .images li").click(
         function(i){
-            if (!$(this).hasClass("selected")) {
-                slideshow_goto( $(this).parents(".fri3d-slideshowwrapper"), $(this).data("volgnr") );
+            if (!$(this).hasClass("selected") && !$(this).parents(".images").hasClass("noclick")) {
+                $(this).parents(".images").data('nextnr', $(this).data('volgnr'));
+                set_srcs( $(this).parents(".fri3d-slideshowwrapper"), $(this).data('volgnr') );
+                slideshow_slide( $(this).parents(".images"), $(this).data('dir') );
             }
-            //set_totalwidth( $(this) );
         }
     );
     $( window ).resize(function(){
