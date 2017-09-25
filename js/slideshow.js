21fb8bde3fe6a466c6c259d686bdd516afa1bdfa
Toon Van de Putte <toon@automaton.be>
2017-09-25 20:12:50 +0200

    slideshow nav & previews
    - functioning dot nav
    - low res preview images
    - implemented slideshow throughout

--- js/slideshow.js
+++ js/slideshow.js
@@ -29,21 +29,53 @@ $(document).ready(function(){
         imageslist.data('leftpos',leftpos);
         imageslist.data('focusnr',focusnr);
         imageslist.children().remove( ".clone" );
-        $( elem ).find(".images li[data-volgnr="+focusnr+"]").addClass("selected");
         $( elem ).find(".images li[data-volgnr=1]").clone(true).appendTo( imageslist ).addClass( "clone" );
         $( elem ).find(".images li[data-volgnr=2]").clone(true).appendTo( imageslist ).addClass( "clone" );
         $( elem ).find(".images li[data-volgnr="+total+"]").clone(true).prependTo( imageslist ).addClass( "clone" );
         $( elem ).find(".images li[data-volgnr="+(total-1)+"]").clone(true).prependTo( imageslist ).addClass( "clone" );
+        navcontainer = $( elem ).children(".slideshow-nav");
+        navcontainer.find(".dot").remove();
+        if (navcontainer) {
+            for (i = 1; i <= total; i++) {
+                var attachedimage = $( elem ).find(".images li[data-volgnr="+i+"]").children("img").attr("src");
+                var nextdot = '<li class="dot"><a href="'+attachedimage+'"  data-volgnr='+i+'><span></span></a></li>';
+                /*nextdot.children("a").click(function() {
+
+                } );*/
+                navcontainer.children(".nav-dots").append(nextdot)
+            }
+            navcontainer.on('click','li a', function(e){
+                e.preventDefault();
+                console.log('you clicked me');
+                slideshow_jump( $(this).parents(".fri3d-slideshowwrapper"), $(this).data('volgnr') );
+            });
+        }
         set_totalwidth( elem );
+        set_fullurls( imageslist.children("li") );
+        set_bgs( imageslist.children("li") );
         set_srcs( elem, focusnr );
         set_leftpos ( elem, focusnr );
         imageslist.css('opacity',1);
+    }
+
+    function set_fullurls( imglist ) {
+        $.each(imglist, function(k,v){
+            $( v ).data('fullurl', get_fullurl( $( v ).children("img").attr("src") ));
+        });
+    }
+
+    function set_bgs( imglist ) {
+        $.each(imglist, function(k,v){
+            $( v ).css('background-image','url('+$( v ).children('img').attr('src')+')');
+        });
     }
 
     function set_leftpos ( elem, focusnr = 1 ) {
         var imageslist = $( elem ).find( ".images" );
         var shiftleft = imageslist.data('leftpos') - imageslist.data('itemwidth')*focusnr - imageslist.data('itemwidth');
         imageslist.css('left',shiftleft);
+        $( elem ).find(".images li[data-volgnr="+focusnr+"]").addClass("selected");
+        set_srcs( elem, focusnr );
         set_navclasses( elem, focusnr );
     }
 
@@ -52,12 +84,14 @@ $(document).ready(function(){
         curritem = $( elem ).find("li[data-volgnr="+focusnr+"]").addClass("selected");
         curritem.prev("li").data('dir','left');
         curritem.next("li").data('dir','right');
+        $( elem ).children(".slideshow-nav").find("a[data-volgnr="+focusnr+"]").addClass("selected");
     }
 
     function clear_tempclasses( elem ){
         $( elem ).find("li").removeClass("selected");
         $( elem ).find("li").removeClass("left");
         $( elem ).find("li").removeClass("right");
+        $( elem ).children(".slideshow-nav").find("a").removeClass("selected");
     }
 
     function set_srcs( elem, center = 1 ) {
@@ -73,9 +107,20 @@ $(document).ready(function(){
         } else {
             rightitem = $( elem ).find("li[data-volgnr="+( center+1 )+"]");
         }
-        centeritem.children("img").attr("src", centeritem.children("img").data("src"));
-        leftitem.children("img").attr("src", leftitem.children("img").data("src"));
-        rightitem.children("img").attr("src", rightitem.children("img").data("src"));
+        /*var centersrc =  get_fullurl( centeritem.children("img").attr("src") );
+        var leftsrc =  get_fullurl( leftitem.children("img").attr("src") );
+        var rightsrc =  get_fullurl( rightitem.children("img").attr("src") );*/
+        centeritem.children("img").attr("src", centeritem.data('fullurl'));
+        leftitem.children("img").attr("src", leftitem.data('fullurl'));
+        rightitem.children("img").attr("src", rightitem.data('fullurl'));
+    }
+
+    function get_fullurl( thumburl = '' ) {
+        if ('' == thumburl) {
+            return '';
+        }
+        var output = thumburl.split("-tn.").join(".");
+        return output;
     }
 
     function slideshow_slide( elem, dir = 'right' ) {
@@ -93,6 +138,10 @@ $(document).ready(function(){
                 o.find(".images").removeClass('noclick');
             }
         );
+    }
+
+    function slideshow_jump( elem, focusnr = 1 ) {
+        set_leftpos( elem, focusnr );
     }
 
     function set_totalwidth( elem ) {
@@ -115,7 +164,6 @@ $(document).ready(function(){
         function(i){
             if (!$(this).hasClass("selected") && !$(this).parents(".images").hasClass("noclick")) {
                 $(this).parents(".images").data('nextnr', $(this).data('volgnr'));
-                set_srcs( $(this).parents(".fri3d-slideshowwrapper"), $(this).data('volgnr') );
                 slideshow_slide( $(this).parents(".images"), $(this).data('dir') );
             }
         }
